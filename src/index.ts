b689ab25ebadcb9141c7bcb9963878e07d2a5269
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-02-11 00:20:12 +0100

    refactor: create browser-dedicated JWT lib

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { NexauthError } from './constants.js'
 import useAuth from './hooks/useAuth.js'
 import getUser from './libs/getUser.js'
 import jwt from './libs/jwt.js'
+import jwtClient from './libs/jwtClient.js'
 import Nexauth from './Nexauth.js'
 
-export { AuthProvider, getUser, jwt, Nexauth, NexauthError, PrismaAdapter, useAuth }
+export { AuthProvider, getUser, jwt, jwtClient, Nexauth, NexauthError, PrismaAdapter, useAuth }
