ad058532be31c4a0cd18119287e5924f5d3657a1
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-02-15 03:49:08 +0100

    feat: add access token auto-refresh before expiration (#16)

--- src/helpers/isJwtPayloadExpired.ts
+++
@@ -1,9 +0,0 @@
-import getUnixTime from './getUnixTime.js'
-
-import type { TokenPayload } from '../types'
-
-export default function isTokenPayloadExpired<P extends TokenPayload = TokenPayload>(payload: P): boolean {
-  const now = getUnixTime()
-
-  return payload.exp <= now
-}
